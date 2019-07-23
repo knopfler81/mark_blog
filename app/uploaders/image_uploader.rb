01ec3ffe73823ccaec3d86cc3a82ee10cf7125e6
nelly duclos <nellyduclos8@gmail.com>
2019-07-23 16:48:07 +0200

    ajoute les calendars

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,31 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  storage :file
+
+  if Rails.env.production? || Rails.env.staging?
+    storage :fog
+  else
+    storage :file
+  end
+
+  version :thumb do
+    process resize_to_fill: [280, 280]
+  end
+
+  def default_url(*args)
+    "/images/fallback/" + [version_name, "random.jpg"].compact.join('_')
+  end
+
+  def cache_dir
+    "#{Rails.root}/spec/support/uploads/tmp"
+  end 
+
+  def store_dir      
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_23_135314) do
+ActiveRecord::Schema.define(version: 2019_07_23_143315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "calendars", force: :cascade do |t|
+    t.json "images"
+    t.string "year"
+    t.string "link"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "concerts", force: :cascade do |t|
     t.string "date"
